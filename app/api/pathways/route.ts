798c3d0642e5755b6818bf71be2fae7415247897
Parth Patel <59846475+parth2323@users.noreply.github.com>
2025-10-30 19:51:50 -0400

    Change URL and Key Name

--- app/api/pathways/route.ts
+++ app/api/pathways/route.ts
@@ -1,8 +1,8 @@
 // app/api/pathways/route.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function GET() {
